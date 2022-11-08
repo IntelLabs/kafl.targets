c9b07710a61f8ffcb9a1146110b565c2cc3de608
Steffen Schulz <steffen.schulz@intel.com>
2022-11-08 13:02:11 -0800

    vmcall scaffolding

---
+++ linux-user/vmcall/src/vmcall.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C)  2022  Intel Corporation. 
+ *
+ * This software and the related documents are Intel copyrighted materials, and
+ * your use of them is governed by the express license under which they were
+ * provided to you ("License"). Unless the License provides otherwise, you may
+ * not use, modify, copy, publish, distribute, disclose or transmit this software
+ * or the related documents without Intel's prior written permission. This
+ * software and the related documents are provided as is, with no express or
+ * implied warranties, other than those that are expressly stated in the License.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+/*
+ * vmcall.c - a helper tool for placing kAFL/Nyx hypercalls
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <nyx_api.h>
+
+
+int main(char **argv, int argc) {
+
+	return 0;
+}
