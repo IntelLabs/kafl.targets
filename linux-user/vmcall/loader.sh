49c9e2db81e8e638de0839a943ea32a474351863
Steffen Schulz <steffen.schulz@intel.com>
2022-11-09 11:46:11 -0800

    vmcall: add is_nyx()

--- linux-user/vmcall/loader.sh
+++ linux-user/vmcall/loader.sh
@@ -2,6 +2,12 @@
 
 cat /proc/cpuinfo |/vmcall hcat
 
-/vmcall hget -x test.bin
+/vmcall is_nyx
+echo "CPU is Nyx: $?" |/vmcall hcat
+
+/vmcall hget -x vmcall
+/vmcall hget test.bin
+
+ls -l |/vmcall hcat
 
 /vmcall habort "return from loader.sh"
