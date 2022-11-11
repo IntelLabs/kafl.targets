4d8a1036e8b7692c3efab1a082eaa35065c945a6
Steffen Schulz <steffen.schulz@intel.com>
2022-11-11 03:13:15 +0100

    vmcall: add hpush, hpanic

--- linux-user/vmcall/loader.sh
+++ linux-user/vmcall/loader.sh
@@ -5,13 +5,18 @@ cat /proc/cpuinfo |/vmcall hcat
 /vmcall is_nyx
 echo "CPU is Nyx: $?" |/vmcall hcat
 
+/vmcall hget -x -o /bin vmcall
+
 mkdir /fuzz
-/vmcall hget -o /fuzz test.bin
-/vmcall hget -x -o /fuzz vmcall
+vmcall hget -o /fuzz test.bin
+vmcall hget -x -o /fuzz foo
 
-/vmcall hcat /fuzz/test.bin
+vmcall hcat /fuzz/test.bin
 
-ls -l | /vmcall hcat
-ls -l /fuzz | /vmcall hcat
+vmcall hpush /fuzz/foo
+vmcall hpush -o "test_XXXXXX" /fuzz/foo
+
+ls -l | vmcall hcat
+ls -l /fuzz | vmcall hcat
 
 /vmcall habort "return from loader.sh"
