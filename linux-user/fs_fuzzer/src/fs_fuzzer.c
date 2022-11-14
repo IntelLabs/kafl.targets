d47c07995ad670247f4baef3b0dfba4352ea3051
Steffen Schulz <steffen.schulz@intel.com>
2022-11-14 23:06:25 +0100

    fs_fuzzer: clang-format

--- linux-user/fs_fuzzer/src/fs_fuzzer.c
+++ linux-user/fs_fuzzer/src/fs_fuzzer.c
@@ -38,10 +38,11 @@ along with QEMU-PT.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "nyx_api.h"
 
-#define KAFL_TMP_FILE	"/tmp/trash"
-#define PAYLOAD_MAX_SIZE (2^16)
+#define KAFL_TMP_FILE "/tmp/trash"
+#define PAYLOAD_MAX_SIZE (2 ^ 16)
 
-static inline void kill_systemd(void){
+static inline void kill_systemd(void)
+{
 	system("systemctl disable systemd-udevd");
 	system("systemctl stop systemd-udevd");
 	system("systemctl stop systemd-udevd-kernel.socket");
@@ -50,46 +51,50 @@ static inline void kill_systemd(void){
 	system("/lib/systemd/systemctl disable systemd-udevd");
 	system("/lib/systemd/systemctl stop systemd-udevd");
 	system("/lib/systemd/systemctl stop systemd-udevd-kernel.socket");
- 	system("/lib/systemd/systemctl stop systemd-udevd-control.socket");
+	system("/lib/systemd/systemctl stop systemd-udevd-control.socket");
 }
 
-static inline uint64_t get_address(char* identifier) {
-    FILE * fp;
-    char * line = NULL;
-    ssize_t read;
-    ssize_t len;
-    char *tmp;
-    uint64_t address = 0x0;
-    uint8_t identifier_len = strlen(identifier);
-
-    fp = fopen("/proc/kallsyms", "r");
-    if (fp == NULL){
-        return address;
-    }
-
-    while ((read = getline(&line, &len, fp)) != -1) {
-        if(strlen(line) > identifier_len && !strcmp(line + strlen(line) - identifier_len, identifier)){
-                address = strtoull(strtok(line, " "), NULL, 16);
-                break;
-        }
-    }
-
-    fclose(fp);
-    if (line){
-        free(line);
-    }
-    return address;
-}
-
-int main(int argc, char** argv)
+static inline uint64_t get_address(char *identifier)
 {
-	struct stat st = {0};
+	FILE *fp;
+	char *line = NULL;
+	ssize_t read;
+	ssize_t len;
+	char *tmp;
+	uint64_t address = 0x0;
+	uint8_t identifier_len = strlen(identifier);
+
+	fp = fopen("/proc/kallsyms", "r");
+	if (fp == NULL) {
+		return address;
+	}
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		if (strlen(line) > identifier_len &&
+		    !strcmp(line + strlen(line) - identifier_len, identifier)) {
+			address = strtoull(strtok(line, " "), NULL, 16);
+			break;
+		}
+	}
+
+	fclose(fp);
+	if (line) {
+		free(line);
+	}
+	return address;
+}
+
+int main(int argc, char **argv)
+{
+	struct stat st = { 0 };
 	int fd, ret;
 	char loopname[4096];
 	int loopctlfd, loopfd, backingfile;
-    	long devnr;
+	long devnr;
 
-	kAFL_payload* payload_buffer = mmap((void*)NULL, PAYLOAD_MAX_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	kAFL_payload *payload_buffer = mmap((void *)NULL, PAYLOAD_MAX_SIZE,
+					    PROT_READ | PROT_WRITE,
+					    MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 	memset(payload_buffer, 0xff, PAYLOAD_MAX_SIZE);
 
 	kill_systemd();
@@ -104,35 +109,38 @@ int main(int argc, char** argv)
 	kAFL_hypercall(HYPERCALL_KAFL_GET_PAYLOAD, (uint64_t)payload_buffer);
 
 	hprintf("printk: %lx\n", get_address("T printk\n"));
-    kAFL_hypercall(HYPERCALL_KAFL_PRINTK_ADDR, get_address("T printk\n"));
+	kAFL_hypercall(HYPERCALL_KAFL_PRINTK_ADDR, get_address("T printk\n"));
 
 	loopfd = open(loopname, O_RDWR);
 	backingfile = open(KAFL_TMP_FILE, O_RDWR | O_CREAT | O_SYNC, 0777);
 	ioctl(loopfd, LOOP_SET_FD, backingfile);
 
-	while(1){
-		
+	while (1) {
 		lseek(backingfile, 0, SEEK_SET);
 		kAFL_hypercall(HYPERCALL_KAFL_NEXT_PAYLOAD, 0);
-		write(backingfile, payload_buffer->data, payload_buffer->size-4);
+		write(backingfile, payload_buffer->data,
+		      payload_buffer->size - 4);
 		ioctl(loopfd, LOOP_SET_CAPACITY, 0);
 
-		kAFL_hypercall(HYPERCALL_KAFL_ACQUIRE, 0);  
+		kAFL_hypercall(HYPERCALL_KAFL_ACQUIRE, 0);
 #ifdef EXT4
-		ret = mount(loopname, "/tmp/a/", "ext4", payload_buffer->data[payload_buffer->size-4], NULL);
+		ret = mount(loopname, "/tmp/a/", "ext4",
+			    payload_buffer->data[payload_buffer->size - 4],
+			    NULL);
 #elif NTFS
-		ret = mount(loopname, "/tmp/a/", "ntfs", payload_buffer->data[payload_buffer->size-4], NULL);
-#elif FAT32 
+		ret = mount(loopname, "/tmp/a/", "ntfs",
+			    payload_buffer->data[payload_buffer->size - 4],
+			    NULL);
+#elif FAT32
 		ret = mount(loopname, "/tmp/a/", "vfat", 0x1, NULL);
 #endif
-		if(!ret){
+		if (!ret) {
 			mkdir("/tmp/a/trash", 0700);
 			stat("/tmp/a/trash", &st);
-        	umount2("/tmp/a", MNT_FORCE);
-        	}
-        	kAFL_hypercall(HYPERCALL_KAFL_RELEASE, 0);
+			umount2("/tmp/a", MNT_FORCE);
+		}
+		kAFL_hypercall(HYPERCALL_KAFL_RELEASE, 0);
 	}
 	close(backingfile);
-    return 0;
+	return 0;
 }
-
