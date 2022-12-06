8d2321c73161dca9ad5635b2e61e20caab645b7a
Steffen Schulz <steffen.schulz@intel.com>
2022-12-06 12:01:39 -0800

    linux-user: ensure we have busybox-static, deprecate gen_sharedir.sh

--- linux-user/scripts/gen_initrd.sh
+++ linux-user/scripts/gen_initrd.sh
@@ -97,10 +97,13 @@ test -d "$TEMPLATE" || fatal "Could not find initrd template folder >>$TEMPLATE<
 BUSYBOX=$(which busybox) || fatal "Could not find busybox - try 'sudo apt install busybox-static'?"
 LDDTREE=$(which lddtree) || fatal "Could not find lddtree - try 'sudo apt install lddtree'?"
 
+$LDDTREE $BUSYBOX |grep -q "interpreter => none" || fatal "Binary at $BUSYBOX is not static. Try 'apt install busybox-static'."
+
 TARGET_INITRD="$(realpath "$1")"; shift;
 TARGET_ROOT="$(mktemp -d)"
 
 test -z "$TARGET_INITRD" && fatal "Output path $TARGET_INITRD is not set. Abort."
+
 
 create_rootfs
 inject_files "$@"
